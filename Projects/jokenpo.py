b6b6d98adb1eacfba290ece30242dffd9dfbf685
Arthur Klein <131549648+arthurklein05@users.noreply.github.com>
2023-04-28 14:27:46 -0300

    Criei a função "menu"
    A função menu irá mostrar na tela as opções de escolha que o jogador terá.

--- Projects/jokenpo.py
+++ Projects/jokenpo.py
@@ -8,3 +8,12 @@ def write(msg):
     print('~' * len(msg))
 
 write('Eu irei jogar Jokenpô com você!')
+
+def menu():
+    print('\033[36m----MENU----')
+    print('1 - Pedra')
+    print('2 - Papel')
+    print('3 - Tesoura')
+    print('------ ------\033[m')
+
+
