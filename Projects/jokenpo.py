edc9157a8a596ae14bdb55bfceb3b19431c5a85d
Arthur Klein <131549648+arthurklein05@users.noreply.github.com>
2023-04-28 14:22:20 -0300

    Update jokenpo.py

--- Projects/jokenpo.py
+++ Projects/jokenpo.py
@@ -1,5 +1,12 @@
+from time import sleep
+import random
+
 def write(msg):
     size = len(msg) + 4
     print('~' * len(msg))
-    print(f'  {msg}  ')
+    print(f' {msg} ')
     print('~' * len(msg))
+
+write('Eu irei jogar Jokenpô com você!')
+sleep(1)
+lista = ['pedra', 'papel', 'tesoura']
