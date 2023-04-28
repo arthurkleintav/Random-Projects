f233e78c41c9f18d79db18e38f443428ebd0bfca
Arthur Klein <131549648+arthurklein05@users.noreply.github.com>
2023-04-28 14:38:28 -0300

    Update jokenpo.py
    Fiz um loop verificando se o usuário digitou uma opção válida. Se sim, encerra o loop. Se não, envia uma mensagem de erro.

--- Projects/jokenpo.py
+++ Projects/jokenpo.py
@@ -10,10 +10,18 @@ def write(msg):
 write('Eu irei jogar Jokenpô com você!')
 
 def menu():
-    print('\033[36m----MENU----')
+    print('\033[36m--- MENU ---')
     print('1 - Pedra')
     print('2 - Papel')
     print('3 - Tesoura')
     print('------ ------\033[m')
 
+sleep(0.5)
 
+menu()
+
+while True:
+    choice = int(input('Sua escolha: '))
+    if choice >=1 and choice <= 3:
+        break
+    print('\033[31mERRO! Opção inválida.\033[m')
